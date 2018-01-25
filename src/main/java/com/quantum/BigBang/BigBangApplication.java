894db60da0f6dafecfe1cc0c3a3d5877c21abcfe
blsmaciag <bartosz.maciag@gft.com>
2018-01-25 21:53:07 +0100

    bing bang project initial commit

---
+++ src/main/java/com/quantum/BigBang/BigBangApplication.java
@@ -0,0 +1,12 @@
+package com.quantum.BigBang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BigBangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BigBangApplication.class, args);
+	}
+}
